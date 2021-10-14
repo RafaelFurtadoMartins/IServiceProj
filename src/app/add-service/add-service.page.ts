55b8fcefbe9cd6ae544bcf761583c8568dda54fd
RafaelFurtadoMartins <67593264+RafaelFurtadoMartins@users.noreply.github.com>
2021-10-14 09:39:59 -0300

    add service

--- src/app/add-service/add-service.page.ts
+++ src/app/add-service/add-service.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-add-service',
   templateUrl: './add-service.page.html',
   styleUrls: ['./add-service.page.scss'],
 })
-export class  implements OnInit {
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
+export class AddServicePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
   }
 
- 
 }
