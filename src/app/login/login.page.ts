32c4329dc6461aca503cd62b4b1d4dcabad7ba53
RafaelFurtadoMartins <67593264+RafaelFurtadoMartins@users.noreply.github.com>
2021-10-15 15:03:37 -0300

    Update login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,91 +1,91 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { AlertController, NavController, NavParams } from '@ionic/angular';
-import { AuthProvider } from '../auth/auth';
-import { FeedPage } from '../feed/feed.page';
+// import { Component, OnInit } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { AlertController, NavController, NavParams } from '@ionic/angular';
+// import { AuthProvider } from '../auth/auth';
+// import { FeedPage } from '../feed/feed.page';
 
 
-@Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
-})
-export class LoginPage {
+// @Component({
+//   selector: 'page-login',
+//   templateUrl: 'login.html',
+// })
+// export class LoginPage {
 
 
-  email:string = '';
-  password:string = '';
+//   email:string = '';
+//   password:string = '';
 
-  errorMsg:string;
+//   errorMsg:string;
 
 
 
-  constructor(public navCtrl: NavController, 
-    public navParams: NavParams,
-    public authService: AuthProvider ,
+//   constructor(public navCtrl: NavController, 
+//     public navParams: NavParams,
+//     public authService: AuthProvider ,
    
-    public alertCtrl: AlertController ,
+//     public alertCtrl: AlertController ,
   
-  ) {
-  }
+//   ) {
+//   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
-
-
-  async errorFunc(message){
-    let alert = this.alertCtrl.create({
-      header: 'Warning!',
-      message: 'error',
-      buttons: ['OK']
-    });
-    (await alert).present();
-  }
+//   ionViewDidLoad() {
+//     console.log('ionViewDidLoad LoginPage');
+//   }
 
 
+//   async errorFunc(message){
+//     let alert = this.alertCtrl.create({
+//       header: 'Warning!',
+//       message: 'error',
+//       buttons: ['OK']
+//     });
+//     (await alert).present();
+//   }
 
 
-logIn(){
+
+
+// logIn(){
  
-    if (this.email.trim() !==''    ) {    
+//     if (this.email.trim() !==''    ) {    
       
-      console.log(this.email.trim() + "   " + this.password.trim() )
+//       console.log(this.email.trim() + "   " + this.password.trim() )
        
-      if (this.password.trim()  === '') {
-        this.errorFunc('Please put your password')
+//       if (this.password.trim()  === '') {
+//         this.errorFunc('Please put your password')
  
-      }else{
+//       }else{
  
-        let credentials = {
-          email: this.email,
-            password: this.password
-        };
+//         let credentials = {
+//           email: this.email,
+//             password: this.password
+//         };
  
         
-         this.authService.login(credentials).then((result) => {
-            console.log(result);
-            this.navCtrl.navigateForward('FeedPage');
+//          this.authService.login(credentials).then((result) => {
+//             console.log(result);
+//             this.navCtrl.navigateForward('FeedPage');
            
-        }, (err) => {
+//         }, (err) => {
      
-            console.log(err);
-            this. errorFunc('Wrong credentials ! try again')
-            console.log("credentials: "+JSON.stringify(credentials))
+//             console.log(err);
+//             this. errorFunc('Wrong credentials ! try again')
+//             console.log("credentials: "+JSON.stringify(credentials))
             
-        });
+//         });
  
-      }
+//       }
       
-   }
-   else{
+//    }
+//    else{
     
-    this. errorFunc('Please put a vaild password !  for ex:(123456)')
+//     this. errorFunc('Please put a vaild password !  for ex:(123456)')
    
-    }
+//     }
  
 
 
-}
+// }
 
 
-}
+// }
