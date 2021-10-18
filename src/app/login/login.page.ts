254f0c8bb97773797411127e2912ced1ec096f1b
RafaelFurtadoMartins <67593264+RafaelFurtadoMartins@users.noreply.github.com>
2021-10-18 09:48:35 -0300

    6

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,72 +7,72 @@ import { FeedPage } from '../feed/feed.page';
 
 
 @Component({
-  selector: 'page-login',
-  templateUrl: 'login.html',
+    selector: 'page-login',
+    templateUrl: 'login.html',
 })
 export class LoginPage {
 
 
-  email:string = '';
-  password:string = '';
-  errorMsg:string;
+    email: string = '';
+    password: string = '';
+    errorMsg: string;
     alertController: any;
 
 
 
-  constructor(public navCtrl: NavController,
-    private apiService: ApiService, 
-    public navParams: NavParams,
-    public authService: AuthenticationService ,
-    public alertCtrl: AlertController ,
-    private router: Router,
-
-  
-  ) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
-  }
+    constructor(public navCtrl: NavController,
+        private apiService: ApiService,
+        public navParams: NavParams,
+        public authService: AuthenticationService,
+        public alertCtrl: AlertController,
+        private router: Router,
 
 
-  async errorFunc(message){
-    let alert = this.alertCtrl.create({
-      header: 'Warning!',
-      message: 'error',
-      buttons: ['OK']
-    });
-    (await alert).present();
-  }
+    ) {
+    }
+
+    ionViewDidLoad() {
+        console.log('ionViewDidLoad LoginPage');
+    }
+
+
+    async errorFunc(message) {
+        let alert = this.alertCtrl.create({
+            header: 'Warning!',
+            message: 'error',
+            buttons: ['OK']
+        });
+        (await alert).present();
+    }
 
 
 
 
-logIn(){
+    logIn() {
 
 
-    return this.apiService
-      .logar({ email: this.email, password: this.password })
-      .subscribe(response => {
-        if (response.success == true) {
-          this.authService.login(response.data);
-          this.router.navigate(["home-page"]);
-        }
+        return this.apiService
+            .logar({ email: this.email, password: this.password })
+            .subscribe(response => {
+                if (response.success == true) {
+                    this.authService.login(response.data);
+                    this.router.navigate(["home-page"]);
+                }
 
-      },
-        error => {
+            },
+                async error => {
 
-               const alert = await this.alertController.create({
-                  cssClass: 'my-custom-class',
-                  header: 'Alert',
-                  subHeader: 'Subtitle',
-                  message: 'This is an alert message.',
-                  buttons: ['OK']
+                    const alert = await this.alertController.create({
+                        cssClass: 'my-custom-class',
+                        header: 'Alert',
+                        subHeader: 'Subtitle',
+                        message: 'This is an alert message.',
+                        buttons: ['OK']
+                    });
+
+
                 });
-        
-
-      );
-  }
+    }
 
 
 }
