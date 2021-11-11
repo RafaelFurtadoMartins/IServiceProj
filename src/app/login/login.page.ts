38f1415fa77a43c90bd7e83c41662040a659c0b9
RafaelFurtadoMartins <67593264+RafaelFurtadoMartins@users.noreply.github.com>
2021-11-11 18:56:25 -0500

    s

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -38,7 +38,7 @@ export class LoginPage implements OnInit {
   }
 
   login(form: NgForm) {
-    this.authService.login(form.value.email, form.value.senha).subscribe(
+    this.authService.login(form.value.email, form.value.password).subscribe(
       data => {
         this.alertService.presentToast("Logged In");
       },
@@ -52,7 +52,6 @@ export class LoginPage implements OnInit {
     );
   }
   register(){
-    
     this.navCtrl.navigateForward("register");
   }
 
